d3358881fab61b1db969a819e935b4fda1b737f0
mikenovum <mikenovum@gmail.com>
2026-08-01 23:11:05 +0300

    fix(badge): добавлен отступ между иконкой и значением в бейдже

--- src/shared/ui/badge/Badge.variants.ts
+++ src/shared/ui/badge/Badge.variants.ts
@@ -1,7 +1,7 @@
 import { cva, type VariantProps } from 'class-variance-authority';
 
 const badgeVariants = cva(
-  'inline-flex items-center rounded-full border font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  'inline-flex items-center gap-1 rounded-full border font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     variants: {
       variant: {
